4a6f4a987e9df57b7fa962a90ca74d506367971a
Saptarshi Sahoo <saptarshi@cmi.ac.in>
2022-08-02 19:53:20 +0530

    Added oddEven and is_Valid

---
+++ exercises.py
@@ -0,0 +1,20 @@
+import sys
+
+def oddeven(x):
+    if x==1: return 1
+    elif x==2: return 0
+    else: return oddeven(x-2)
+
+def is_Valid(x):
+    if x == 1 and x==2:
+        return True
+    elif x<1:
+        return False
+    else:
+        if x%2==1:
+            return is_Valid(x-3)
+        else:
+            return is_Valid(x-3) or is_Valid(x/2)
+
+# print(oddeven(int(sys.argv[1])))
+print(is_Valid(int(sys.argv[1])))
