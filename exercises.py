b1fe5c51710f1711591678914e9ed22ca1a0bd54
Saptarshi Sahoo <saptarshi@cmi.ac.in>
2022-08-07 16:46:52 +0530

    set1 problem 14

--- exercises.py
+++ exercises.py
@@ -5,6 +5,14 @@ def oddeven(x):
     elif x==2: return 0
     else: return oddeven(x-2)
 
+def is_valid(a,b):
+	if a==0 and b==0: 
+		return True
+	elif a<0 or b<0:
+		return False
+	else: 
+		return is_valid(a,b-1) or is_valid(a-1,b-1) or is_valid(a-2, b-1)
+
 def is_Valid(x):
     if x == 1 and x==2:
         return True
@@ -17,4 +25,5 @@ def is_Valid(x):
             return is_Valid(x-3) or is_Valid(x/2)
 
 # print(oddeven(int(sys.argv[1])))
-print(is_Valid(int(sys.argv[1])))
+# print(is_Valid(int(sys.argv[1])))
+print(is_valid(int(sys.argv[1]),int(sys.argv[2])))
